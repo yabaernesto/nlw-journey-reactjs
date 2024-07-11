6b1dc7eed74ecca2ce45747e77379a9f71297b8a
Yaba Ernesto <yabaernesto@gmail.com>
2024-07-11 22:30:31 +0100

    aula 3 finalizada

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,4 +1,4 @@
-import { CircleDashed, UserCog } from "lucide-react";
+import { CheckCircle2, CircleDashed, UserCog } from "lucide-react";
 import { Button } from "../../components/Button";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -23,16 +23,20 @@ export function Guests() {
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Convidados</h2>
       <div className="space-y-5">
-        {participants.map(participant => {
+        {participants.map((participant, index) => {
           return (
             <div key={participant.id} className="flex items-center justify-between gap-4">
               <div className="space-y-1.5">
-                <span className="block font-medium text-zinc-100">{participant.name}</span>
+                <span className="block font-medium text-zinc-100">{participant.name ?? `Convidado ${index}`}</span>
                 <span className="block text-sm text-zinc-400 truncate">
                   {participant.email}
                 </span>
               </div>
-              <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+              {participant.is_confirmed ? (
+                <CheckCircle2 className="size-5 shrink-0 text-green-400" />
+              ): (
+                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+              )}
             </div>
           )
         })}
